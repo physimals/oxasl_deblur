b75ce2deaf61e5bab7c5df45ff71ec80f730d13f
Martin Craig <martin.craig@eng.ox.ac.uk>
2021-03-02 14:37:06 +0000

    Support option to load kernel in from file - this does not require mask or residuals Also some style tidyups

--- oxasl_deblur/deblur.py
+++ oxasl_deblur/deblur.py
@@ -34,42 +34,39 @@ from oxasl.options import AslOptionParser, OptionCategory, OptionGroup, GenericO
 
 from ._version import __version__, __timestamp__
 
-def threshold(arr, thresh, useabs=False, binarise=False):
+def threshold(arr, thresh_value, useabs=False, binarise=False):
     """
     Threshold an array
 
     :param arr: Array to threshold
-    :param thresh: Threshold value - all values below this are zeroed
+    :param thresh_value: Threshold value - all values below this are zeroed
     :param useabs: If True, threshold based on absolute value
     :param binarise: If True, set all non-zeroed values to 1
     """
     if useabs:
         arr = np.absolute(arr)
        
-    arr[arr < thresh] = 0
-    if binarise: arr[arr >= thresh] = 1
+    arr[arr < thresh_value] = 0
+    if binarise: arr[arr >= thresh_value] = 1
     return arr
 
-def flattenmask(mask, thr):
+def flatten_mask(mask, thresh_value):
     """
     Create a 2D array whose values are 1 if there are at least
-    ``thr`` unmasked voxels in the z direction, 0 otherwise.
+    ``thresh_value`` unmasked voxels in the z direction, 0 otherwise.
     """
-    if thr > mask.shape[2]:
+    if thresh_value > mask.shape[2]:
         raise RuntimeError("Cannot flatten mask with a threshold larger than the z dimension")
 
-    # Set all unmasked voxels to 1
     mask = np.copy(mask)
     mask[mask > 0] = 1
+    return threshold(np.sum(mask, 2), thresh_value, binarise=True)
 
-    return threshold(np.sum(mask, 2), thr, binarise=True)
-
-def Zvols2matrix(data, mask):
+def zvols_to_matrix(data, mask):
     """
-    Takes 4D volume and 2D (xy) or 3D (xyt) mask and return 2D matrix
-    (space-time x z-dimension)
-    
-    Just vols2matrix but choosing the Z-dimension
+    :param data: 4D Numpy array
+    :param mask: Mask flattened in Z dimension (may have optional T dimension)
+    :return Masked data as 2D Numpy array - first dimension is XYT, second is Z
     """
     # Mask is 2D need to repeat by number of t points
     if mask.ndim == 2:
@@ -82,13 +79,13 @@ def Zvols2matrix(data, mask):
     mask = np.reshape(mask, [mask.size]) > 0
     
     # need to swap axes so 2nd dim of 2D array is Z not T
-    data = np.transpose(data, [0, 1, 3, 2])
-    data2 = np.reshape(data, [mask.size, data.shape[3]])
+    data = np.transpose(data, [0, 1, 3, 2]) # Shape [X, Y, T, Z]
+    data2 = np.reshape(data, [mask.size, data.shape[3]]) # Shape [X*Y*T, Z]
     return data2[mask, :]
 
 def zdeblur_make_spec(resids, flatmask):
-    zdata = Zvols2matrix(resids, flatmask)
-    ztemp = np.zeros(zdata.shape)
+    # Mask and de-mean residuals
+    zdata = zvols_to_matrix(resids, flatmask)
     mean = zdata.mean(axis=1, dtype=np.float64)
     ztemp = zdata - mean[:, np.newaxis]
     
@@ -121,7 +118,7 @@ def lorentzian_wiener(length, gamma, tunef):
 def gaussian_autocorr(length, sig):
     """
     Returns the autocorrelation function for Gaussian smoothed white
-    noise with length data points, where the Gaussian std dev is sigma 
+    noise with length data points, where the Gaussian std dev is sigma
     
     For now we go via the gaussian fourier transform
     (autocorr is ifft of the power spectral density)
@@ -137,7 +134,7 @@ def gaussian_autocorr(length, sig):
 def gaussian_fft(sig, length, demean=True):
     """
     Returns the fourier transform function for Gaussian smoothed white
-    noise with len data points, where the Gaussian std dev is sigma 
+    noise with length data points, where the Gaussian std dev is sigma
     """
     tres = 1.0
     fres = 1.0/(tres*length)
@@ -153,18 +150,23 @@ def fit_gaussian_autocorr(thefft):
     """
     Fit a Gaussian autocorrelation model to the data and return the
     std dev sigma
-    """
 
-    # (autocorr is ifft of the power spectral density)
+    (autocorr is ifft of the power spectral density)
+    """
     data_raw_autocorr = np.real(ifft(np.square(np.absolute(thefft))))
     data_raw_autocorr = data_raw_autocorr/max(data_raw_autocorr)
 
     popt, _ = curve_fit(gaussian_autocorr, len(data_raw_autocorr), data_raw_autocorr, 1)
     return popt[0]
 
-def create_deblur_kern(thefft, kernel, kernlen, sig=1):
+def create_deblur_kern(thefft, kernel_name, kernel_length, sig=1):
+    """
+    Create the deblurring kernel
+
+    :param kernel_name: Kernel name
+    """
     np.set_printoptions(precision=16)
-    if kernel == "direct":
+    if kernel_name == "direct":
         slope = thefft[1]-thefft[2]
         thefft[0] = thefft[1]+slope #put the mean in for tapering of the AC
         thefft = thefft/(thefft[1]+slope) #normalise, we want DC=1, but we will have to extrapolate as we dont ahve DC
@@ -174,39 +176,39 @@ def create_deblur_kern(thefft, kernel, kernlen, sig=1):
         t1 = 1-tukey(len(thefft), sig)
         thefft = np.sqrt(np.absolute(fft(np.multiply(i1, t1))))
         thefft[0] = 0 # back to zero mean
-    elif kernel == "lorentz":
+    elif kernel_name == "lorentz":
         ac = np.real(ifft(np.square(thefft))) # autocorrelation
         ac = ac/max(ac)
         popt, _ = curve_fit(lorentzian_autocorr, len(ac), ac, 2)
         gamma = popt[0]
-        lozac = lorentzian_autocorr(kernlen, gamma)
+        lozac = lorentzian_autocorr(kernel_length, gamma)
         lozac = lozac/max(lozac)
-        thefft = np.absolute(fft(lorentzian_kern(gamma, kernlen, True))) # when getting final spec. den. include mean        
-    elif kernel == "lorwien":
+        thefft = np.absolute(fft(lorentzian_kern(gamma, kernel_length, True))) # when getting final spec. den. include mean
+    elif kernel_name == "lorwien":
         ac = np.real(ifft(np.square(thefft))) # autocorrelation
         ac = ac/max(ac)
         popt, _ = curve_fit(lorentzian_wiener, len(ac), ac, (2, 0.01))
         gamma, tunef = popt
-        lozac = lorentzian_wiener(kernlen, gamma, tunef)
-        thefft = np.absolute(fft(lorentzian_kern(gamma, kernlen, True))) # when getting final spec. den. include mean
+        lozac = lorentzian_wiener(kernel_length, gamma, tunef)
+        thefft = np.absolute(fft(lorentzian_kern(gamma, kernel_length, True))) # when getting final spec. den. include mean
         thepsd = np.square(thefft)
         tune = tunef*np.mean(thepsd)
         wien = np.divide(thepsd, thepsd+tune)
         wien[0] = 1
         thefft = np.divide(thefft, wien)
-    elif kernel == "gauss":
+    elif kernel_name == "gauss":
         sigfit = fit_gaussian_autocorr(thefft)
-        thefft = gaussian_fft(sigfit, kernlen, True) # When getting final spec. den. include mean
-    elif kernel == "manual":
-        if len(sig) != kernlen:
-            raise RuntimeError("Manual deblur kernel requires signal of length %i" % kernlen)
-        thefft = gaussian_fft(sig, kernlen, True)
+        thefft = gaussian_fft(sigfit, kernel_length, True) # When getting final spec. den. include mean
+    elif kernel_name == "manual":
+        if len(sig) != kernel_length:
+            raise RuntimeError("Manual deblur kernel requires signal of length %i" % kernel_length)
+        thefft = gaussian_fft(sig, kernel_length, True)
     else:
-        raise RuntimeError("Unknown kernel: %s" % kernel)
+        raise RuntimeError("Unknown kernel: %s" % kernel_name)
 
     # note that currently all the ffts have zero DC term!
     invkern = np.reciprocal(np.clip(thefft[1:], 1e-50, None))
-    kern = np.real(ifft(np.insert(invkern, 0, 0)))
+    kernel = np.real(ifft(np.insert(invkern, 0, 0)))
     
     # Code below is commented out in MATLAB original - preserving for now
 
@@ -218,34 +220,36 @@ def create_deblur_kern(thefft, kernel, kernlen, sig=1):
     # The ffts should be already correctly normalized (unity DC)
 
     # normalise
-    #if sum(kern)>0.01
-    #   kern = kern/(sum(kern))
+    #if sum(kernel)>0.01
+    #   kernel = kernel/(sum(kernel))
     #else
     #    warning('normalization of kernel skipped')
     #end
 
-    if len(kern) < kernlen:
+    if len(kernel) < kernel_length:
         # if the kernel is shorter than required pad in the middle by zeros
-        n = kernlen-len(kern)
-        i1 = int(len(kern)/2)
-        kern = np.concatenate((kern[:i1], np.zeros(n), kern[i1:]))
-    return kern
+        n = kernel_length-len(kernel)
+        i1 = int(len(kernel)/2)
+        kernel = np.concatenate((kernel[:i1], np.zeros(n), kernel[i1:]))
+    return kernel
    
-def zdeblur_with_kern(volume, kern, deblur_method="fft"):
-    if deblur_method == "fft":
+def zdeblur_with_kern(volume, kernel, deblur_method="fft"):
+    """
+    Deblur an image
 
-        # FIXME MATLAB code below transposes and takes complex conjugate 
-        # We don't need to transpose, so just take conjugate. However not 
+    :param volume: 4D Numpy array containing data to be deblurred
+    :param kernel: Numpy array containing deblurring kernel
+    :param deblur_method: Name of method to use with deblurring
+    """
+    if deblur_method == "fft":
+        # FIXME original MATLAB transposes and takes complex conjugate
+        # We don't need to transpose, so just take conjugate. However not
         # completely clear if complex conjugate is required or if this is
         # an unintentional side-effect of the transpose
-        fftkern = np.conj(fft(kern))
-        #if size(fftkern,2)==1
-        #    fftkern = fftkern'
-        #end
+        fftkern = np.conj(fft(kernel))
 
-        # demean volume (in z) - 'cos kern is zero mean
-        m = np.expand_dims(np.mean(volume, 2), 2)
-        zmean = np.repeat(m, volume.shape[2], 2)
+        # Demean volume along Z axis - kernel is zero mean
+        zmean = np.expand_dims(np.mean(volume, 2), 2)
         volume = volume  - zmean
         
         fftkern = np.expand_dims(fftkern, 0)
@@ -259,7 +263,7 @@ def zdeblur_with_kern(volume, kern, deblur_method="fft"):
         return volout
 
     elif deblur_method == "lucy":
-        #volout = filter_matrix(volume, kern)
+        #volout = filter_matrix(volume, kernel)
         raise RuntimeError("Lucy-Richardson deconvolution not supported in this version of ASL_DEBLUR")
     else:
         raise RuntimeError("Unknown deblur method: %s" % deblur_method)
@@ -312,50 +316,30 @@ def get_residuals(wsp):
     """
     Run BASIL on ASL data to get residuals prior to deblurring
     """
-    wsp.log.write(' - Running BASIL to generate residuals\n')
-    wsp.sub("basil")
-    wsp.basil_options = {
-        "save-residuals" : True,
-    #    "inferart" : False,
-    #    "spatial" : False,
-    }
-    basil.basil(wsp, output_wsp=wsp.basil)
-    wsp.residuals = wsp.basil.finalstep.residuals
+    if wsp.residuals is not None:
+        wsp.log.write(' - Residuals already supplied\n')
+    else:
+        wsp.log.write(' - Running BASIL to generate residuals\n')
+        wsp.sub("basil")
+        wsp.basil_options = {
+            "save-residuals" : True,
+            "inferart" : False,
+            "spatial" : False,
+        }
+        basil.basil(wsp, output_wsp=wsp.basil)
+        wsp.residuals = wsp.basil.finalstep.residuals
+
+def get_mask(wsp):
+    if wsp.mask is None:
+        mask.generate_mask(wsp.deblur)
+        wsp.mask = wsp.rois.mask
+    else:
+        wsp.mask = wsp.mask
 
 def run(wsp, output_wsp=None):
     """
     Run deblurring on an OXASL workspace
-    """
-    wsp.sub("deblur")
-    if output_wsp is None:
-        output_wsp = wsp.deblur
-    wsp.log.write('\nDeblurring data\n')
 
-    if wsp.mask is None:
-        mask.generate_mask(wsp.deblur)
-        wsp.deblur.mask = wsp.deblur.rois.mask
-    else:
-        wsp.deblur.mask = wsp.mask
-
-    if wsp.residuals is None:
-        get_residuals(wsp.deblur)
-
-    output_wsp.asldata = deblur_img(wsp.deblur, wsp.asldata)
-    if wsp.calib is not None:
-        output_wsp.calib = deblur_img(wsp.deblur, wsp.calib)
-    if wsp.addimg is not None:
-        output_wsp.addimg = deblur_img(wsp.deblur, wsp.addimg)
-    # FIXME CATC, CBLIP...
-
-    wsp.log.write('DONE\n')
-
-def deblur_img(wsp, img):
-    """
-    Do deblurring on ASL data
-
-    :param img: Image to deblur. If AslImage is provided, an AslImage is returned
-    :return: Image or AslImage
-    
     Required workspace attributes
     -----------------------------
 
@@ -368,44 +352,86 @@ def deblur_img(wsp, img):
      - ``mask`` : Data mask. If not provided, will be auto generated
      - ``residuals`` : Residuals from model fit on ASL data. If not specified and ``wsp.asldata``
                        is provided, will run BASIL fitting on this data to generate residuals
+     - ``kernel`` : Numpy array containing pre-generated kernel
+    """
+    wsp.log.write('\nDeblurring data\n')
+    wsp.sub("deblur")
+    if output_wsp is None:
+        output_wsp = wsp.deblur
+
+    get_kernel(wsp.deblur)
+
+    wsp.log.write(' - Deblur method: %s\n' % wsp.deblur_method)
+    output_wsp.asldata = deblur_img(wsp.deblur, wsp.asldata)
+    if wsp.calib is not None:
+        output_wsp.calib = deblur_img(wsp.deblur, wsp.calib)
+    if wsp.addimg is not None:
+        output_wsp.addimg = deblur_img(wsp.deblur, wsp.addimg)
+    # FIXME CATC, CBLIP...
+
+    wsp.log.write('DONE\n')
+
+def data_pad(img, padding_slices=2):
+    """
+    :param img: Image in ASL data space
+
+    :return: 4D Numpy array of data in an image, padded by 2 slices top and bottom
     """
     # Ensure data is 4D by padding additional dimension if necessary
-    deblur_data = img.data
-    if deblur_data.ndim == 3:
-        deblur_data = deblur_data[..., np.newaxis]
+    data = img.data
+    if data.ndim == 3:
+        data = data[..., np.newaxis]
 
     # Pad the data - 2 slices top and bottom
-    data_pad = np.pad(deblur_data, [(0, 0), (0, 0), (2, 2), (0, 0)], 'edge')
+    return np.pad(data, [(0, 0), (0, 0), (padding_slices, padding_slices), (0, 0)], 'edge')
  
-    if wsp.kernel is None:
-        wsp.log.write(' - Using kernel: %s\n' % wsp.deblur_kernel)
-        wsp.log.write(' - Deblur method: %s\n' % wsp.deblur_method)
+def data_unpad(padded_data, padding_slices=2):
+    """
+    :param padded_data: Numpy array of data padded by 2 slices top and bottom
+    :return: 4D Numpy array with padding discarded
+    """
+    return padded_data[:, :, padding_slices:-padding_slices, :]
+
+def get_kernel(wsp):
+    if wsp.kernel is not None:
+        wsp.log.write(' - Kernel already supplied\n')
+    else:
         # Number of slices that are non zero in mask
+        get_mask(wsp)
         maskser = np.sum(wsp.mask.data, (0, 1))
         nslices = np.sum(maskser > 0)
-        flatmask = flattenmask(wsp.mask.data, nslices-2)
+        flatmask = flatten_mask(wsp.mask.data, nslices-2)
 
         # Commented out in MATLAB code
         # residser = zdeblur_make_series(resids,flatmask)
+        get_residuals(wsp)
         thespecd = zdeblur_make_spec(wsp.residuals.data, flatmask)
 
         # NB data has more slices than residuals
+        wsp.log.write(' - Using kernel: %s\n' % wsp.deblur_kernel)
+        data_padded = data_pad(wsp.asldata)
         sig = wsp.ifnone("sig", 1)
-        wsp.kernel = create_deblur_kern(thespecd, wsp.deblur_kernel, data_pad.shape[2], sig)
+        wsp.kernel = create_deblur_kern(thespecd, wsp.deblur_kernel, data_padded.shape[2], sig)
 
-    # Do deblurring
+def deblur_img(wsp, img):
+    """
+    Deblur an image
+
+    :param wsp: Workspace
+    :param img: Image to be deblurred
+    :return Deblurred Image
+    """
     wsp.log.write(' - Deblurring image %s\n' % img.name)
-    dataout = zdeblur_with_kern(data_pad, wsp.kernel, wsp.deblur_method)
-    
-    # Discard padding and return
-    dataout = dataout[:, :, 2:-2, :]
+    data_padded = data_pad(img)
+    data_deblurred = zdeblur_with_kern(data_padded, wsp.kernel, wsp.deblur_method)
+    data_out = data_unpad(data_deblurred)
     if img.data.ndim == 3:
-        dataout = np.squeeze(dataout, axis=3)
+        data_out = np.squeeze(data_out, axis=3)
 
     if isinstance(img, AslImage):
-        ret = img.derived(dataout)
+        ret = img.derived(data_out)
     else:
-        ret = Image(dataout, header=img.header)
+        ret = Image(data_out, header=img.header)
 
     return ret
 
@@ -425,6 +451,7 @@ class Options(OptionCategory):
                               "'lorentz' - Lorentzian kernel, estimate size from data"
                               "'lorwein' - Lorentzian kernel with weiner type filter", 
                          choices=["direct", "gauss", "manual", "lorentz", "lorwein"], default="direct")
+        group.add_option("--kernel-file", dest="kernel", help="File containing pre-specified deblurring kernel data", type="matrix")
         group.add_option("--method", dest="deblur_method", 
                          help="Deblurring method: Choicess are 'fft' for division in FFT domain or 'lucy' for Lucy-Richardson (ML solution) for Gaussian noise", 
                          choices=["fft", "lucy"], default="fft")
@@ -467,7 +494,7 @@ def main():
             wsp.deblur = None
             wsp.input = None
 
-        print('\nOXASL_DEBLUR - DONE - output is %s' % options.output)
+        print('\nOXASL_DEBLUR - DONE - output is in %s' % options.output)
 
     except RuntimeError as e:
         print("ERROR: " + str(e) + "\n")
